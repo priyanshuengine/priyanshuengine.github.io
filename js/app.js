fdb48acd48cb354a439d9c1f40f8d829c0080c71
Priyanshu Chaturvedi <chaturvedi.priyanshu11@gmail.com>
2022-08-07 23:23:15 +0530

    UPD COMMIT

---
+++ js/app.js
@@ -0,0 +1,186 @@
+const filter_btns = document.querySelectorAll(".filter-btn");
+const skills_wrap = document.querySelector(".skills");
+const skills_bars = document.querySelectorAll(".skill-progress");
+const records_wrap = document.querySelector(".records");
+const records_numbers = document.querySelectorAll(".number");
+const footer_input = document.querySelector(".footer-input");
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".links a");
+
+
+footer_input.addEventListener("focus", () => {
+  footer_input.classList.add("focus");
+});
+
+footer_input.addEventListener("blur", () => {
+  if (footer_input.value != "") return;
+  footer_input.classList.remove("focus");
+});
+
+function closeMenu() {
+  navbar.classList.remove("open");
+  document.body.classList.remove("stop-scrolling");
+}
+
+
+hamburger_menu.addEventListener("click", () => {
+  if (!navbar.classList.contains("open")) {
+    navbar.classList.add("open");
+    document.body.classList.add("stop-scrolling");
+  } else {
+    closeMenu();
+  }
+});
+
+links.forEach((link) => link.addEventListener("click", () => closeMenu()));
+
+filter_btns.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    filter_btns.forEach((button) => button.classList.remove("active"));
+    btn.classList.add("active");
+
+    let filterValue = btn.dataset.filter;
+
+    $(".grid").isotope({ filter: filterValue });
+  })
+);
+
+$(".grid").isotope({
+  itemSelector: ".grid-item",
+  layoutMode: "fitRows",
+  transitionDuration: "0.6s",
+});
+
+window.addEventListener("scroll", () => {
+  skillsEffect();
+  countUp();
+});
+
+function checkScroll(el) {
+  let rect = el.getBoundingClientRect();
+  if (window.innerHeight >= rect.top + el.offsetHeight) return true;
+  return false;
+}
+
+function skillsEffect() {
+  if (!checkScroll(skills_wrap)) return;
+  skills_bars.forEach((skill) => (skill.style.width = skill.dataset.progress));
+}
+
+function countUp() {
+  if (!checkScroll(records_wrap)) return;
+  records_numbers.forEach((numb) => {
+    const updateCount = () => {
+      let currentNum = +numb.innerText;
+      let maxNum = +numb.dataset.num;
+      let speed = 100;
+      const increment = Math.ceil(maxNum / speed);
+
+      if (currentNum < maxNum) {
+        numb.innerText = currentNum + increment;
+        setTimeout(updateCount, 1);
+      } else {
+        numb.innerText = maxNum;
+      }
+    };
+
+    setTimeout(updateCount, 400);
+  });
+}
+
+var mySwiper = new Swiper(".swiper-container", {
+  speed: 1100,
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  navigation: {
+    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
+  },
+});
+$(document).ready(function(){
+
+  $(window).scroll(function(){
+
+      if(this.scrollY > 200){
+          $('.navbar').addClass("sticky");
+          }else{
+              $('.navbar').removeClass("sticky");
+              }
+
+        // Scrolling Button Btn
+        if(this.scrollY >500){
+          $('.scroll-up-btn').addClass("show");
+          }else{
+          $('.scroll-up-btn').removeClass("show");
+          }   
+  });
+
+  var typed = new Typed(".typing-1",{
+      strings: [
+          "Web Developer", "Android Developer",
+          "UX Designer", "Freelancer"
+      ],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+  });
+  var typed = new Typed(".typing-2",{
+      strings: [
+        "Web Developer", "Android Developer","UX Designer",
+        "Freelancer"
+      ],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+  });
+
+  //slide-up script
+  $('.scroll-up-btn').click(function(){
+      $('html').animate({scrollTop:0});
+  });
+
+  // owl carousel animation
+  $('.carousel').owlCarousel({
+      margin: 20,
+      loop:true,
+      navigation:true,
+      autoplay: true,
+      autoplayTimeOut: 1000,
+      autoplayHoverPause: true,
+
+      responsive: {
+          0:{
+              items:1,
+              nav:false
+          },
+          600:{
+              items: 2,
+              nav: false
+          },
+          1000:{
+              items: 3,
+              nav: false
+          }
+      }
+  });
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
